417d595514f3d56b50295a7d09d20bfce1c473dd
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-06-07 00:26:14 +0300

    chore: remove root package.json and configure Vercel deployment settings with CORS policies

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,11 +1,15 @@
-// const allowedOrigins = ["http://localhost:8080", "https://domain.vercel.app"];
+const allowedOrigins = ["https://kashf-ai-electricity-assistant.vercel.app"];
 const corsOptions = {
     origin: (origin, callback) => {
-        // if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-        // } else {
-        // callback(new Error("Access denied! Not allowed by CORS"));
-        // }
+        if (process.env.NODE_ENV === "development") {
+            callback(null, true);
+        } else {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Access denied! Not allowed by CORS"));
+            }
+        }
     },
     credentials: true, // Allow cookies/authorization headers
 };
